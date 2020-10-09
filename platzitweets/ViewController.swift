2f127898875e49549e291c1096f6ff94c31c80c8
Omar Flores Grimontt <dev@omarefg.com>
2020-10-09 15:43:21 -0500

    initial commit

---
+++ platzitweets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  platzitweets
+//
+//  Created by Omar Flores on 9/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
